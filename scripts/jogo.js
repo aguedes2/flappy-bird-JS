517a1f2dc54220fa5b7746ad083d2c30d099a7fc
aguedes2 <aguedes2@gmail.com>
2022-12-27 17:28:18 -0300

    Criação e transicao de telas: INICIO, JOOG, GAME OVER. Mudança de estados do flappy

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -11,6 +11,7 @@ const flappyBird = {
   altura: 24,
   x: 10,
   y: 50,
+  isDead: false,
   velocidade: 0,
   gravity: 0.25,
   draw() {
@@ -26,16 +27,31 @@ const flappyBird = {
       this.altura //tamanho da imagem a printar
     )
   },
-  atualiza() {
-    if (this.y + this.altura < canvas.height) {
+  update() {
+    this.dead()
+    if (this.y + this.altura < canvas.height && !this.isDead) {
       this.velocidade += this.gravity
       this.y += this.velocidade
     }
   },
   pula() {
-    addEventListener('click', () => {
-      this.y -= 20
-    })
+    if (!this.isDead) {
+      addEventListener('click', () => {
+        this.y -= 20
+        this.velocidade = 0
+      })
+    }
+  },
+  dead() {
+    if (this.y + this.velocidade >= canvas.height - 112) {
+      this.isDead = true
+      mudaTela(Telas.GAME_OVER)
+    }
+  },
+  init() {
+    this.isDead = false
+    this.y = 50
+    this.velocidade = 0
   }
 }
 
@@ -109,15 +125,105 @@ const fundo = {
   }
 }
 
+///mensagem GetReady
+const mensagemGetReady = {
+  spriteX: 134,
+  spriteY: 0,
+  largura: 174,
+  altura: 152,
+  x: canvas.width / 2 - 87,
+  y: canvas.height * 0.25,
+  draw() {
+    ctx.drawImage(
+      spritesheet,
+      this.spriteX,
+      this.spriteY,
+      this.largura,
+      this.altura,
+      this.x,
+      this.y,
+      this.largura,
+      this.altura
+    )
+  }
+}
+
+const mensagemGameOver = {
+  spriteX: 134,
+  spriteY: 152,
+  largura: 256,
+  altura: 201,
+  x: canvas.width / 2 - 110,
+  y: canvas.height * 0.2,
+  draw() {
+    ctx.drawImage(
+      spritesheet,
+      this.spriteX,
+      this.spriteY,
+      this.largura,
+      this.altura,
+      this.x,
+      this.y,
+      this.largura,
+      this.altura
+    )
+  }
+}
+
+/**
+ * Telas
+ */
+let telaAtiva = {}
+function mudaTela(novaTela) {
+  telaAtiva = novaTela
+}
+const Telas = {
+  INICIO: {
+    draw() {
+      Telas.JOGO.draw()
+      mensagemGetReady.draw()
+    },
+    update() {
+      flappyBird.init()
+      addEventListener('click', () => {
+        mudaTela(Telas.JOGO)
+      })
+    }
+  },
+  JOGO: {
+    draw() {
+      fundo.draw()
+      chao.draw()
+      flappyBird.draw()
+    },
+    update() {
+      flappyBird.update()
+    }
+  },
+  GAME_OVER: {
+    draw() {
+      fundo.draw()
+      chao.draw()
+      flappyBird.draw()
+      mensagemGameOver.draw()
+    },
+    update() {
+      addEventListener('click', () => {
+        mudaTela(Telas.INICIO)
+      })
+    }
+  }
+}
+
 function loop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-  fundo.draw()
-  chao.draw()
-  flappyBird.draw()
-  flappyBird.atualiza()
+  telaAtiva.draw()
+  telaAtiva.update()
 
   requestAnimationFrame(loop)
 }
 
+mudaTela(Telas.INICIO)
+flappyBird.pula()
 loop()
